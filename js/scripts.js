ea46223bde06a2d72afb093b3b0bb3e454c7434d
Marialafu <lafuentelhmaria@gmail.com>
2025-04-11 11:20:35 +0200

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,97 +1,110 @@
 //si la tipografía la pongo al body no me lo cogen los botones, ¿se pone entonces la tipografía en el button también?
-//los contadores se pueden hacer en un H3 o es necesario que sea en un input? así estaría bien?
-//Hay alguna manera de que al borrar todos los datos de un input aparezca de nuevo el placeholder original sin tener que volver a ponerlo?
+
+//contadores mejor e un span, no son titulos.
+
 //como se organizaría el css para que esté más optimizado. El botón de rules tiene todo pero podría coger cosas de otro.
-
-//Igual que en el var se pondría hand-circle en vez de blue-circle. ¿Es mejor si la clase le ponemos el nombre de hand?
-
-//Primero coloqué el blue, luego el red, y cuando coloqué el amarillo se me movió el rojo y tuve que recolocarlo, ¿por qué?
-//¿Si esto se ve en otra pantalla puede que se descoloque todo?
 
 //Para el background tuve que poner altura y anchura y lo puse con vh y wh. Pero si no ponía uno de los dos me hacía scroll hasta que tuviera contenido, no? Se quedó raro y ahora si lo quito funciona.
 
 //¿Por qué no me funciona el font family en los buttons. Al principio si pero se ha quitado solo.
 
-//¿Como coloco los 3 circulos originales, tendría que crear una clase para el simple y otra para el advanced?
-
-
 const startSimpleGameElement = document.getElementById('start-simple-game');
+const duringSimpleGameElement = document.getElementById('during-simple-game');
 const playAgainButtonElement = document.getElementById('play-again-button');
 
 const youPickedElement = document.getElementById('you-picked');
-const gameResultMessageElement = document.getElementById('game-result-message')
-const scoreResultElement = document.getElementById('score-number')
+const userPickedElement = document.getElementById('game-picked');
+
+const imageYouPickedElement = document.getElementById('image-you-picked');
+const imageGamePickedElement = document.getElementById('image-game-picked');
+
+const gameResultMessageElement = document.getElementById('game-result-message');
+const scoreYouResultElement = document.getElementById('score-you');
+const scorePCResultElement = document.getElementById('score-pc');
 
 const houseOptions = ['scissors', 'paper', 'rock', 'lizard', 'spock'];
 
 let userPickedOption;
 let housePickedOption;
-let scoreResult = 0
+
+let scoreYouResult = 0;
+let scorePcResult = 0;
 
 const winOptions = {
   scissors: {
     paper: true,
     rock: false,
     lizard: true,
-    spock: false,
+    spock: false
   },
-  paper:{
+  paper: {
     scissors: false,
-    rock:true,
+    rock: true,
     spock: true,
-    lizard: false,
+    lizard: false
   },
-  rock:{
+  rock: {
     lizard: true,
     spock: false,
     scissors: true,
-    paper: false,
+    paper: false
   },
-  lizard:{
+  lizard: {
     spock: true,
     scissors: false,
     paper: true,
-    rock: false,
+    rock: false
   },
   spock: {
     lizard: false,
     scissors: true,
     rock: true,
-    paper: false,
+    paper: false
   }
-}
+};
 
-const comparePickedOptions = (event) => {
+const imagesOptions = {
+  scissors: './assests/images/icon-scissors.svg',
+  paper: './assests/images/icon-paper.svg',
+  rock: './assests/images/icon-rock.svg',
+  lizard: './assests/images/icon-lizard.svg',
+  spock: './assests/images/icon-spock.svg'
+};
 
+const comparePickedOptions = event => {
   if (userPickedOption === housePickedOption) {
-    gameResultMessageElement.textContent = 'NO SCORE'
-  } else if (winOptions[userPickedOption][housePickedOption] === true) {
-    gameResultMessageElement.textContent = 'YOU WIN'
-    scoreResult++
+    gameResultMessageElement.textContent = 'NO SCORE';
+  } else if (winOptions[userPickedOption][housePickedOption]) {
+    gameResultMessageElement.textContent = 'YOU ARE AWESOME';
+    scoreYouResult++;
   } else {
-    gameResultMessageElement.textContent = 'YOU LOSE'
+    gameResultMessageElement.textContent = 'YOU ARE FUCKED UP';
+    scorePcResult++;
   }
-  scoreResultElement.textContent = scoreResult
-  playAgainButtonElement.parentElement.classList.remove('hide')
+  scoreYouResultElement.textContent = scoreYouResult;
+  scorePCResultElement.textContent = scorePcResult;
+  duringSimpleGameElement.classList.remove('hide');
 };
 
 const saveHousePickedOption = () => {
   const randomIndex = Math.floor(Math.random() * houseOptions.length);
   housePickedOption = houseOptions[randomIndex];
+  imageGamePickedElement.src = imagesOptions[housePickedOption];
+
   comparePickedOptions();
 };
-//guardar mi jugada - en you picked
+
 const saveUserPickedOption = event => {
   userPickedOption = event.target.dataset.icon;
-  startSimpleGameElement.classList.add('hide')
-
+  startSimpleGameElement.classList.add('hide');
+  imageYouPickedElement.src = imagesOptions[event.target.dataset.icon];
   saveHousePickedOption();
 };
 
 const reestartGame = () => {
-  playAgainButtonElement.parentElement.classList.add('hide')
-  startSimpleGameElement.classList.remove('hide')
-}
+  duringSimpleGameElement.classList.add('hide');
+  startSimpleGameElement.classList.remove('hide');
+};
 
 startSimpleGameElement.addEventListener('click', saveUserPickedOption);
 playAgainButtonElement.addEventListener('click', reestartGame);
